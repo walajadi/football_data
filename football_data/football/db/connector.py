989bc7f3b939af0a6a5f279831dd8a646349462d
slim <=>
2017-11-08 17:37:10 +0100

    set db connection

---
+++ football_data/football/db/connector.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import psycopg2
+import sys
+
+class DBConnection(object):
+
+	def __init__(self):
+
+		self.conn = None
+
+	def set_conn(self, dbname='testdb', user='user_1'):
+		"""
+		"""
+		try:
+			self.con = psycopg2.connect(dbname=dbname, user=user_1)
+			return self.con.cursor() 
+		except psycopg2.DatabaseError as e:
+			print('Error occured at Connection : {}'.format(e))
+
+		return None
+
+
+
+
